6c21c041e5ea278a28a99ca7251d9c2b90004ee5
Yaqing Wang <yaqingwang@users.noreply.github.com>
2022-01-01 17:00:47 -0500

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,2 @@
 #bash scripts/train_SpanNER.sh train conll03_gold all_avg_linear 128 16 16 true 1 bert-base-uncased 10 3 ontonotes5
-bash scripts/train_SpanNER.sh train conll03_gold all_avg_linear 128 8 16 true 0 bert-base-uncased 10
+bash scripts/train_SpanNER.sh train conll03_gold all_avg_linear 128 16 16 true 0 bert-base-uncased 10
